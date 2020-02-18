2c12744d1a5a4bf5ae1860e94661c6ec2bde1853
Arseniy Gromovoy <arseniygromovoy@gmail.com>
2020-02-18 13:10:58 +0200

    Additional test added

--- src/streamsApiTest/TestCases.java
+++ src/streamsApiTest/TestCases.java
@@ -21,4 +21,17 @@ public class TestCases {
         assertEquals(expected, actual);
 
     }
+
+    @Test
+    public void testCase2() {
+        int[] input = new int[] {-7, 12, 137, -105, 0, 3, 27, 999};
+        Map<Integer, List<String>> expected = Map.of(
+                1, List.of("e0", "o3"),
+                2, List.of("e12", "o27"),
+                3, List.of("o137", "o999")
+        );
+        Map<Integer, List<String>> actual = IntGrouper.groupByDigitNumbers(input);
+
+        assertEquals(expected, actual);
+    }
 }
